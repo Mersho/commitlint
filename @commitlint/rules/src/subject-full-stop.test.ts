2e229c1947a46ba20619953cf72ff29c16be5d32
Mersho <code.rezaei@gmail.com>
2023-08-15 15:24:24 +0330

    test: a failing test on subjectFullStop
    Titles that end with an ellipsis should fail on "always." testcase.

--- @commitlint/rules/src/subject-full-stop.test.ts
+++ @commitlint/rules/src/subject-full-stop.test.ts
@@ -80,3 +80,9 @@ test('commit title ends with ellipsis against "never ." should succeed', async (
 	const expected = true;
 	expect(actual).toEqual(expected);
 });
+
+test('commit title ends with triple dot against "always ." should succeed', async () => {
+	const [actual] = subjectFullStop(await parsed.ellipsisMessage, 'always', '.');
+	const expected = true;
+	expect(actual).toEqual(expected);
+});
